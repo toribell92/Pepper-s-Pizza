bba7d9e01b270c379a732f5653a75183aaabf8c0
toribell <liyvb2@nottingham.ac.uk>
2016-05-10 11:01:19 +0100

    Java Script

---
+++ script.js
@@ -0,0 +1,6 @@
+var greet = function (){
+    var greeting = "Hello";
+    var name = prompt("What's your name?");
+    var greeting_message = greeting + name + "!";
+    alert (greeting_message);
+}
