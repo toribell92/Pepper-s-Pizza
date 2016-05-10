7e13927f99e5310487ee53dbaf60707435b7b5a1
toribell <liyvb2@nottingham.ac.uk>
2016-05-10 11:23:04 +0100

    Generate Message Function

--- script.js
+++ script.js
@@ -1,6 +1,14 @@
-var greet = function (){
-    var greeting = "Hello";
+var generateMessage = function (salutation, visitor_name) {
+    var salutation_message = salutation + visitor_name;
+    return salutation_message;
+
+};
+
+var greet = function (event_details){
+    // Gets the user's name and greets them
+    var greeting = "Hello ";
     var name = prompt("What's your name?");
-    var greeting_message = greeting + name + "!";
+    var greeting_message = generateMessage (greeting, name);
     alert (greeting_message);
-}
+};
+greet();
