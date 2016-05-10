52a5afe23c3f6d81f31108ddb9272c76a78a4f96
toribell <liyvb2@nottingham.ac.uk>
2016-05-10 12:19:03 +0100

    Connnect Java to Form

--- script.js
+++ script.js
@@ -1,14 +1,22 @@
-var generateMessage = function (salutation, visitor_name) {
-    var salutation_message = salutation + visitor_name;
-    return salutation_message;
+// var generateMessage = function (salutation, visitor_name) {
+//     var salutation_message = salutation + visitor_name;
+//     return salutation_message;
+//
+// };
+//
+// var greet = function (event_details){
+//     // Gets the user's name and greets them
+//     var greeting = "Hello ";
+//     var name = prompt("What's your name?");
+//     var greeting_message = generateMessage (greeting, name);
+//     alert (greeting_message);
+// };
+// greet();
 
+var greet = function(event_details) {
+    alert ("You are now signed up for our Newsletter!");
+    event_details.preventDefault();
 };
 
-var greet = function (event_details){
-    // Gets the user's name and greets them
-    var greeting = "Hello ";
-    var name = prompt("What's your name?");
-    var greeting_message = generateMessage (greeting, name);
-    alert (greeting_message);
-};
-greet();
+var form = document.getElementById('contact-form');
+form.addEventListener('submit', greet);
